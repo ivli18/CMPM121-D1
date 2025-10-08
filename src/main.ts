1df11cd1ee2062228a9a133e2062b93d0f488c7f
ivli18 <161982162+ivli18@users.noreply.github.com>
2025-10-08 20:49:19 +0000

    Add click handler to button and live-updating counter display (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let count = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <button> 🦎
+  <button id="lbutton"> 🦎
+  <div id="counter"> 0 lizards </div>
+
 `;
+
+const button = document.getElementById("lbutton") as HTMLButtonElement;
+const countDisplay = document.getElementById("counter") as HTMLDivElement;
+
+
+button.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = `${count} lizards`
+})
